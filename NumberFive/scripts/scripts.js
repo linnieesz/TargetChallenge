73ce737b52c0f129753fa69d3a4d3b1055076e0e
linnieesz <86932868+linnieesz@users.noreply.github.com>
2025-01-20 17:46:53 -0300

    added NumberFive UI

--- NumberFive/scripts/scripts.js
+++ NumberFive/scripts/scripts.js
@@ -1,18 +1,17 @@
-// Função para inverter os caracteres de uma string
 function inverterString(str) {
   let stringInvertida = '';
-  
-  // Percorre a string de trás para frente
   for (let i = str.length - 1; i >= 0; i--) {
     stringInvertida += str[i];
   }
-
   return stringInvertida;
 }
 
-// Exemplo de uso
-let stringOriginal = "Exemplo de string";
-let stringInvertida = inverterString(stringOriginal);
+function atualizarInvertida() {
+  const stringOriginal = document.getElementById('inputOriginal').value;
+  
+  const stringInvertida = inverterString(stringOriginal);
 
-console.log("String original:", stringOriginal);
-console.log("String invertida:", stringInvertida);
+  document.getElementById('inputInvertida').value = stringInvertida;
+}
+
+document.getElementById('invertButton').addEventListener('click', atualizarInvertida);
